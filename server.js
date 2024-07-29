220eda8690d8dc2321fd77064a2d4e687ce6b598
BharathSevarkodiyon <bharathsevarkodiyon2000@gmail.com>
2024-07-29 11:57:10 +0530

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 // Create a basic HTTP server in Node.js that returns "Hello, World!"
 
-let http = require('http')  // it a inbuild module, here we are importing the module inside the require.
+let http = require('http')  // it a inbuild module
 let port = 5000;
 
-let server = http.createServer((req, res)=>{   //
+let server = http.createServer((req, res)=>{
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("Hello world!!!"); // it will be visible on UI
-    res.end();                   // it is mandatory to give end(), because if it not there the page is keep on loading and also we can give the string to diplay on the UI
+    res.write("Hello world!!!");
+    res.end();
 })
-
 server.listen((port), ()=>{
     console.log(`Server is running at the port number ${port}`);
 })
